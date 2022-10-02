44f8b28cf57a579fe5bda95e3e86707a4c65b341
Janita Williamson <nitabusiacct@gmail.com>
2022-10-02 04:20:08 +0000

    pushing code

---
+++ Week12_Proj.py
@@ -0,0 +1,13 @@
+#creating a python list
+
+#task 1 empty list
+my_servicelist=[]
+my_servicelist = ['machine learning','ec2','s3']
+print ("List of String:", my_servicelist)
+print("The length of the list is:", len(my_servicelist))
+del my_servicelist[2]
+print ("List of String:", my_servicelist)
+del my_servicelist [1]
+print ("List of String:", my_servicelist)
+print("The length of the list is:", len(my_servicelist))
+print(len(my_servicelist))
